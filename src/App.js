53b59ed0bb6691664af65b3218270489656784a2
taegyuni <taegyun7553@naver.com>
2022-10-09 00:25:58 +0900

    DiaryList 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import "./App.css";
-import DiaryEditor from "./DiaryEditor";
+import './App.css';
+import DiaryEditor from './DiaryEditor';
+import DiaryList from './DiaryList';
 
+const dummyList = [{
+  id : 1,
+  author:"태균",
+  content:"하이",
+}];
 function App() {
   return (
     <div className="App">
       <DiaryEditor />
+      <DiaryList />
     </div>
   );
 }
