17a716300e413aa155225462cb5ac571c6d8deb9
taegyuni <taegyun7553@naver.com>
2022-10-09 19:35:18 +0900

    fix

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
 
-const dummyList = [{
-  id : 1,
-  author:"태균",
-  content:"하이",
-}];
+const dummyList = [
+  {
+    id: 1,
+    author: '태균',
+    content: '하이',
+  },
+];
 function App() {
   return (
     <div className="App">
