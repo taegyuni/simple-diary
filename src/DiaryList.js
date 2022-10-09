dc7c1703cd06c0a680ca40bc99fd7d4c37f24cc8
taegyuni <taegyun7553@naver.com>
2022-10-09 12:52:26 +0900

    DiaryList 수정

--- src/DiaryList.js
+++ src/DiaryList.js
@@ -1,9 +1,24 @@
-const DiaryList = () => {
+const DiaryList = ({diaryList}) => {
+  console.log(diaryList)
   return (
     <div classname="DiaryList">
       <h2>일기 리스트</h2>
+      <h4>{diaryList.length}개의 일기가 있습니다.</h4>
+      <div>
+        {diaryList.map((it) => (
+          <div>
+            <div>작성자 : {it.author}</div>
+            <div>일기 : {it.content}</div>
+            <div>감정 : {it.emotion}</div>
+            <div>작성시간 : {it.created_date}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
+DiaryList.defaultProps={
+  diaryList: [],
+}
 
 export default DiaryList;
