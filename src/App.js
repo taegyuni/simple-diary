b17b6f4e46b1c567929d9916e0c364267690a090
taegyuni <taegyun7553@naver.com>
2022-10-09 21:02:47 +0900

    data 추가하기

--- src/App.js
+++ src/App.js
@@ -1,43 +1,60 @@
+import { useRef, useState } from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
 
-const dummyList = [
-  {
-    id: 1,
-    author: '태균',
-    content: '하이 1',
-    emotion: 5,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 2,
-    author: '명훈',
-    content: '하이 2',
-    emotion: 4,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 3,
-    author: '솔',
-    content: '하이 3',
-    emotion: 2,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 4,
-    author: '영희',
-    content: '하이 4',
-    emotion: 3,
-    created_date: new Date().getTime(),
-  },
-];
+// const dummyList = [
+//   {
+//     id: 1,
+//     author: '태균',
+//     content: '하이 1',
+//     emotion: 5,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 2,
+//     author: '명훈',
+//     content: '하이 2',
+//     emotion: 4,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 3,
+//     author: '솔',
+//     content: '하이 3',
+//     emotion: 2,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 4,
+//     author: '영희',
+//     content: '하이 4',
+//     emotion: 3,
+//     created_date: new Date().getTime(),
+//   },
+// ];
 
 function App() {
+  const [data, setDate] = useState([]);
+
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current,
+    };
+    dataId.current += 1;
+    setDate([newItem, ...data]);
+  };
   return (
     <div className="App">
-      <DiaryEditor />
-      <DiaryList diaryList={dummyList} />
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList diaryList={data} />
     </div>
   );
 }
