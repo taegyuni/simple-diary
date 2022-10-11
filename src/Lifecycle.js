c10d4a06def61d643d6964402cd2852dddf982ba
taegyuni <taegyun7553@naver.com>
2022-10-11 21:55:09 +0900

    feat : useEffect 기능활용

--- src/Lifecycle.js
+++ src/Lifecycle.js
@@ -1,32 +1,26 @@
 import react, { useEffect, useState } from "react";
 
+//두개 이상의 컴포넌트가 같은 파일에 포함되어도 됨.
+const UnmountTest = () => {
+  useEffect(() => {
+    console.log("Mount!");
+
+    return () => {
+      //Unmount 시점에 실행됨.
+      console.log("Unmount!");
+    };
+  }, []);
+  return <div>Unmount Testing Component</div>;
+};
+
 const Lifecycle = () => {
-  const [count, setCount] = useState(0);
-  const [text, setText] = useState("");
-
-  useEffect(() => {
-    console.log("Mount");
-  }, []);
-
-  useEffect(() => {
-    console.log("update");
-  }, []);
+  const [isVisible, setISVisible] = useState(false);
+  const toggle = () => setISVisible(!isVisible);
 
   return (
     <div style={{ padding: 20 }}>
-      <div>
-        {count}
-        <button
-          onClick={() => {
-            setCount(count + 1);
-          }}
-        >
-          +
-        </button>
-      </div>
-      <div>
-        <input value={text} onChange={(e) => setText(e.target.value)} />
-      </div>
+      <button onClick={toggle}>ON/OFF</button>
+      {isVisible && <UnmountTest />}
     </div>
   );
 };
